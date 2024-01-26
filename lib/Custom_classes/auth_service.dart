f974513a91d53077b4ab44f675163853430036b5
Paul Botchwey <botchweypaul0001@gmail.com>
2024-01-26 23:41:06 +0000

    Implemented the Login Persistence

--- lib/Custom_classes/auth_service.dart
+++ lib/Custom_classes/auth_service.dart
@@ -1,97 +1,95 @@
 // auth_service.dart
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-  final FlutterSecureStorage secureStorage = FlutterSecureStorage();
+  final String baseUrl = 'https://ethenatx.pythonanywhere.com/management';
+  late String _accessToken;
+  late String _refreshToken;
 
-  String? accessToken;
-  String? refreshToken;
+  // Singleton pattern to ensure only one instance of AuthService
+  static final AuthService _instance = AuthService._internal();
 
-  Future<void> logInAndGetTokens(String email, String password,
-      {bool isRefresh = false}) async {
-    // Check if it's the first-time user
-    if (!isRefresh) {
-      // If it's the first time, check if there's a stored token
-      accessToken = await secureStorage.read(key: 'access_token');
-      refreshToken = await secureStorage.read(key: 'refresh_token');
-      print('Refresh: $refreshToken');
-    }
-
-    final apiUrl = Uri.parse(
-        'https://ethenatx.pythonanywhere.com/management/obtain-token/');
-
-    try {
-      final response = await http.post(
-        apiUrl,
-        headers: {'Content-Type': 'application/json; charset=UTF-8'},
-        body: jsonEncode(
-          isRefresh
-              ? {'refresh': refreshToken}
-              : {'email': email, 'password': password},
-        ),
-      );
-      print('Login or Refresh Response: ${response.statusCode}');
-      print('Response Body: ${response.body}');
-
-      if (response.statusCode == 200) {
-        final jsonResponse = json.decode(response.body) as Map<String, dynamic>;
-        accessToken = jsonResponse['access'];
-        if (!isRefresh) {
-          refreshToken = jsonResponse['refresh'];
-        }
-        print(accessToken);
-        print(refreshToken);
-        await _storeTokens();
-      } else if (response.statusCode == 401 && !isRefresh) {
-        // Token expired, try refreshing
-        if (await _refreshAccessToken()) {
-          // Retry login after refresh
-          await logInAndGetTokens('', '', isRefresh: true);
-        }
-      } else {
-        accessToken = null;
-        refreshToken = null;
-      }
-    } catch (e) {
-      accessToken = null;
-      refreshToken = null;
-    }
+  factory AuthService() {
+    return _instance;
   }
 
-  Future<bool> _refreshAccessToken() async {
+  AuthService._internal();
+
+  // Getter for the access token
+  String get accessToken => _accessToken;
+
+  // Check if the user is authenticated
+  bool isAuthenticated() {
+    return _accessToken.isNotEmpty;
+  }
+
+  // Log in and obtain access and refresh tokens
+  Future<bool> login(String email, String password) async {
     try {
       final response = await http.post(
-        Uri.parse(
-            'https://ethenatx.pythonanywhere.com/management/refresh-token/'),
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode({
-          'refresh': refreshToken,
-          // 'email': '', // Add empty values for email and password
-          // 'password': '',
-        }),
+        Uri.parse('$baseUrl/obtain-token/'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'email': email, 'password': password}),
       );
 
       if (response.statusCode == 200) {
-        final jsonResponse = json.decode(response.body) as Map<String, dynamic>;
-        accessToken = jsonResponse['access'];
-        await _storeTokens();
+        final data = json.decode(response.body);
+        _accessToken = data['access'];
+        _refreshToken = data['refresh'];
+        await _saveTokensToStorage();
         return true;
       } else {
-        accessToken = null;
-        refreshToken = null;
+        // Handle login error
         return false;
       }
     } catch (e) {
+      // Handle network or other errors
       return false;
     }
   }
 
-  Future<void> _storeTokens() async {
-    await secureStorage.write(key: 'access_token', value: accessToken!);
-    await secureStorage.write(key: 'refresh_token', value: refreshToken!);
+  // Refresh the access token using the refresh token
+  Future<bool> refreshToken() async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/refresh-token/'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'refresh': _refreshToken}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        _accessToken = data['access'];
+        await _saveTokensToStorage();
+        return true;
+      } else {
+        // Handle refresh token error
+        return false;
+      }
+    } catch (e) {
+      // Handle network or other errors
+      return false;
+    }
+  }
+
+  // Save tokens to local storage (SharedPreferences)
+  Future<void> _saveTokensToStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('accessToken', _accessToken);
+    prefs.setString('refreshToken', _refreshToken);
+  }
+
+  // Load tokens from local storage (SharedPreferences)
+  Future<void> _loadTokensFromStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    _accessToken = prefs.getString('accessToken') ?? '';
+    _refreshToken = prefs.getString('refreshToken') ?? '';
+  }
+
+  // Initialize the AuthService
+  Future<void> init() async {
+    await _loadTokensFromStorage();
   }
 }
